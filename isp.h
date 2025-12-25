3594187f7d9ec48e8bbaed8825fb98e9b9d0f521
Alecs <alecshome.ah@gmail.com>
2025-12-25 17:15:38 +0300

    Add files via upload
    Оптимизация

---
+++ isp.h
@@ -0,0 +1,95 @@
+/*
+ * isp.h - part of USBasp
+ *
+ * Autor..........: Thomas Fischl <tfischl@gmx.de>
+ * Description....: Provides functions for communication/programming
+ *                  over ISP interface
+ * Licence........: GNU GPL v2 (see Readme.txt)
+ * Creation Date..: 2005-02-23
+ * Last change....: 2009-02-28
+ */
+
+#ifndef ISP_H
+#define ISP_H
+
+#ifndef __isp_h_included__
+#define	__isp_h_included__
+
+#ifndef uchar
+#define	uchar	unsigned char
+#endif
+
+#define	ISP_OUT   PORTB
+#define ISP_IN    PINB
+#define ISP_DDR   DDRB
+#define ISP_RST   PB2
+#define ISP_MOSI  PB3
+#define ISP_MISO  PB4
+#define ISP_SCK   PB5
+
+// Для поддержки всех AVR нужно правильно определить:
+#define FLASH_MAX_BYTES   (512UL*1024)    // Максимум для USBasp
+#define MAX_EEPROM_SIZE   (64UL * 1024)   // 64KB - максимум 16-битного адреса
+#define EXTADDR_BLOCK     (0x20000UL)     // 0x20000UL=128K граница и для ATmega2560 (256KB = 0x40000).
+#define EXTADDR_BLOCKS    (FLASH_MAX_BYTES / EXTADDR_BLOCK)
+
+#define CS_LOW()	ISP_OUT &= ~(1 << ISP_RST); /* RST low */
+#define CS_HI()		ISP_OUT |= (1 << ISP_RST); /* RST high */
+
+extern uchar (*ispTransmit)(uchar);
+
+/* Prepare connection to target device */
+void ispConnect();
+
+void isp25Connect();
+
+/* Close connection to target device */
+void ispDisconnect();
+
+void ispDelay();
+
+/* read an write a byte from isp using software (slow) */
+uchar ispTransmit_sw(uchar send_byte);
+
+/* read an write a byte from isp using hardware (fast) */
+uchar ispTransmit_hw(uchar send_byte);
+
+/* enter programming mode */
+uchar ispEnterProgrammingMode();
+
+/* read byte from eeprom at given address */
+uchar ispReadEEPROM(unsigned int address);
+
+/* write byte to flash at given address */
+uchar ispWriteFlash(uint32_t address, uchar data, uchar pollmode);
+
+uchar ispFlushPage(uint32_t address);
+
+/* read byte from flash at given address */
+uchar ispReadFlash(uint32_t address);
+
+/* write byte to eeprom at given address */
+uchar ispWriteEEPROM(unsigned int address, uchar data);
+
+/* pointer to sw or hw transmit function */
+uchar (*ispTransmit)(uchar);
+
+/* set SCK speed. call before ispConnect! */
+void ispSetSCKOption(uchar option);
+
+/* load extended address byte */
+void ispLoadExtendedAddressByte(uint32_t address);
+
+void spibusy(void);
+
+uchar ispReadFlashRaw(uint32_t address);
+
+void ispUpdateExtended(uint32_t address);
+
+extern uint8_t last_success_speed; // объявление, а не определение
+extern uchar is_connected;
+extern uchar isp_hiaddr;
+
+#endif /* __isp_h_included__ */
+
+#endif
